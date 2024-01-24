7c10ac697c99a61471948cbf1c89d9a833832165
freetimezp <kraftukrnet@ukr.net>
2024-01-24 19:11:25 +0200

    02. js-social-18-instagram. Auth page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+//chakra theme
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props)
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ config, styles });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,30 @@
+import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react";
+
+import AuthForm from "../components/AuthForm";
+
+function AuthPage() {
+    return (
+        <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+            <Container maxW={"container.md"} padding={0}>
+                <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+                    <Box display={{ base: "none", md: "block" }}>
+                        <Image src="/auth.png" alt="auth" h={650} />
+                    </Box>
+
+                    <VStack spacing={4} align={"stretch"}>
+                        <AuthForm />
+                        <Box textAlign={"center"}>
+                            Get the app.
+                        </Box>
+                        <Flex gap={5} justifyContent={"center"}>
+                            <Image src="/playstore.png" alt="play store" h={10} />
+                            <Image src="/microsoft.png" alt="microsoft" h={10} />
+                        </Flex>
+                    </VStack>
+                </Flex>
+            </Container>
+        </Flex>
+    );
+}
+
+export default AuthPage;
