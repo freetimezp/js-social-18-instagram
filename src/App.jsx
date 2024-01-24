7d72d427c7b867ff8d6234ffaabba0c9b8895ff6
freetimezp <kraftukrnet@ukr.net>
2024-01-24 20:35:53 +0200

    03. js-social-18-instagram. Navbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 
+import PageLayout from "./layouts/PageLayout";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   );
 }
 
