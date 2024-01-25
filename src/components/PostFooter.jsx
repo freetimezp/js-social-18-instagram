0e70f2a93905771f78ff28841a46b3720a529aec
freetimezp <kraftukrnet@ukr.net>
2024-01-25 22:01:36 +0200

    09. js-social-18-instagram. ProfilePost component. Modal.

--- src/components/PostFooter.jsx
+++ src/components/PostFooter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react";
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../assets/constants";
 
-function PostFooter({ username }) {
+function PostFooter({ username, isProfilePage }) {
     const [liked, setLiked] = useState(false);
     const [likes, setLikes] = useState(1000);
 
@@ -17,7 +17,7 @@ function PostFooter({ username }) {
     };
 
     return (
-        <Box mb={10}>
+        <Box mb={10} marginTop={"auto"}>
             <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={6}>
                 <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
                     {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
@@ -32,16 +32,20 @@ function PostFooter({ username }) {
                 {likes} likes
             </Text>
 
-            <Text fontSize={"sm"} fontWeight={700}>
-                {username}{" "}
-                <Text as="span" fontWeight={400}>
-                    Have a nice day..
-                </Text>
-            </Text>
+            {!isProfilePage && (
+                <>
+                    <Text fontSize={"sm"} fontWeight={700}>
+                        {username}{" "}
+                        <Text as="span" fontWeight={400}>
+                            Have a nice day..
+                        </Text>
+                    </Text>
 
-            <Text fontSize={"sm"} color={"gray"}>
-                View all 1,000 comments
-            </Text>
+                    <Text fontSize={"sm"} color={"gray"}>
+                        View all 1,000 comments
+                    </Text>
+                </>
+            )}
 
             <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
                 <InputGroup>
--- src/components/ProfilePost.jsx
+++ src/components/ProfilePost.jsx
@@ -1,18 +1,51 @@
-import { Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import {
+    Avatar,
+    Box,
+    Divider,
+    Flex,
+    GridItem,
+    Image,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalOverlay,
+    Text,
+    VStack,
+    useDisclosure
+} from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+import Comment from "./Comment";
+import PostFooter from "./PostFooter";
 
 function ProfilePost({ img }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
-            <GridItem cursor={"pointer"} borderRadius={4} overflow={"hidden"} border={"1px solid"}
-                borderColor={"whiteAlpha.300"} position={"relative"} aspectRatio={1 / 1}
-                onClick={onOpen}>
-                <Flex opacity={0} _hover={{ opacity: 1 }} position={"absolute"} top={0} left={0}
-                    right={0} bottom={0} bg={"blackAlpha.700"} transition={"all 0.4s ease-in-out"}
-                    zIndex={1} justifyContent={"center"}>
+            <GridItem
+                cursor={"pointer"}
+                borderRadius={4}
+                overflow={"hidden"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                position={"relative"}
+                aspectRatio={1 / 1}
+                onClick={onOpen}
+            >
+                <Flex
+                    opacity={0}
+                    _hover={{ opacity: 1 }}
+                    position={"absolute"}
+                    top={0} left={0} right={0} bottom={0}
+                    bg={"blackAlpha.700"}
+                    transition={"all 0.4s ease-in-out"}
+                    zIndex={1}
+                    justifyContent={"center"}
+                >
                     <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
                         <Flex>
                             <AiFillHeart size={20} />
@@ -26,16 +59,86 @@ function ProfilePost({ img }) {
                     </Flex>
                 </Flex>
 
-                <Image src={img} alt="profile post" w={"100%"} h={"100%"} objectFit={"cover"} />
+                <Image
+                    src={img} alt="profile post"
+                    w={"100%"} h={"100%"}
+                    objectFit={"cover"}
+                />
             </GridItem>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+                isCentered={true}
+                size={{ base: "3xl", md: "5xl" }}
+            >
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal title</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>
-                        modal body
+                    <ModalBody bg={"black"} pb={5}>
+                        <Flex
+                            gap={4}
+                            mx={"auto"}
+                            w={{ base: "90%", sm: "70%", md: "full" }}
+                        >
+                            <Box
+                                borderRadius={4}
+                                overflow={"hidden"}
+                                border={"1px solid"}
+                                borderColor={"whiteAlpha.300"}
+                                flex={1.5}
+                            >
+                                <Image src={img} alt="post" />
+                            </Box>
+                            <Flex
+                                flex={1}
+                                flexDirection={"column"}
+                                px={10}
+                                display={{ base: "none", md: "flex" }}
+                            >
+                                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                                    <Flex alignItems={"center"} gap={4}>
+                                        <Avatar src="/profilepic.png" size={"sm"} name="Freetime" />
+                                        <Text fontWeight={"bold"} fontSize={12}>Freetime</Text>
+                                    </Flex>
+
+                                    <Box
+                                        _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                                        borderRadius={4}
+                                        p={1}
+                                    >
+                                        <MdDelete size={20} cursor={"pointer"} />
+                                    </Box>
+                                </Flex>
+
+                                <Divider my={4} bg={"gray.500"} />
+
+                                <VStack w={"full"} alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
+                                    <Comment
+                                        createdAt="1d ago"
+                                        username="Freetime"
+                                        profilePic="/profilepic.png"
+                                        text="Dummy image from unsplash"
+                                    />
+                                    <Comment
+                                        createdAt="12d ago"
+                                        username="Abramov"
+                                        profilePic="https://bit.ly/dan-abramov"
+                                        text="Some comment 1"
+                                    />
+                                    <Comment
+                                        createdAt="3h ago"
+                                        username="Kenny"
+                                        profilePic="https://bit.ly/kent-c-dodds"
+                                        text="Some comment 2"
+                                    />
+                                </VStack>
+
+                                <Divider my={4} bg={"gray.800"} />
+
+                                <PostFooter isProfilePage={true} />
+                            </Flex>
+                        </Flex>
                     </ModalBody>
                 </ModalContent>
             </Modal>
