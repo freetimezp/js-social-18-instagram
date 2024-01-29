110cb756e7adc7197fb935790046397bb75c09da
freetimezp <kraftukrnet@ukr.net>
2024-01-29 16:41:40 +0200

    23. js-social-18-instagram. Create post func.

---
+++ src/store/postStore.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+const usePostStore = create((set) => ({
+    posts: [],
+    //create post
+    createPost: (post) => set(state => ({ posts: [post, ...state.posts] })),
+    //delete post
+    //comment 
+    //set posts
+}));
+
+export default usePostStore;
